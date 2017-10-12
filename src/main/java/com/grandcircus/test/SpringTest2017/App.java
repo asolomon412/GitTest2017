efa9ea130cf12599123c840f2b9070764c863b0e
Antonella Solomon <antonellasolomon@gmail.com>
2017-10-12 17:18:16 -0400

    first commit

---
+++ src/main/java/com/grandcircus/test/SpringTest2017/App.java
@@ -0,0 +1,13 @@
+package com.grandcircus.test.SpringTest2017;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
